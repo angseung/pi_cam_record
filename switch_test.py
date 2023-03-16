150ecfb5003a1db330af0e8019dea0719cca7ec1
angseung <swim@surromind.ai>
2023-03-16 09:20:36 +0900

    Added camera shutter codes

--- switch_test.py
+++ switch_test.py
@@ -1,19 +1,21 @@
-import RPi.GPIO as GPIO
 import time
-import keyboard
+from datetime import datetime
+import RPi.GPIO as GPIO
+from picamera2 import Picamera2
 
 btnPin = 18
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(btnPin, GPIO.IN, GPIO.PUD_UP)
 
+picam2 = Picamera2()
+
 while True:
     GPIO.wait_for_edge(btnPin, GPIO.RISING, bouncetime=100)
-    # time.sleep(0.1)
-    #print(GPIO.input(btnPin))
+    time.sleep(1.0)
+    print(GPIO.input(btnPin))
+
     if GPIO.input(btnPin) == 0:
         print("switch pushed")
-
-    key = keyboard.read_key()
-    if key in ["q", "Q"]:
-        break
+        now = str(datetime.now())
+        picam2.start_and_capture_file(f"test_{now}.jpg")

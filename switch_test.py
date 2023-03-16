abc2a048635b7e21c4e6ede2c725b5be8d02dffc
angseung <swim@surromind.ai>
2023-03-16 09:04:33 +0900

    Added break codes

--- switch_test.py
+++ switch_test.py
@@ -1,21 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-
-switch = 18
-
-# GPIO.setmode(GPIO.BCM)
-#
-# GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# GPIO.add_event_detect(switch, GPIO.FALLING, callback=LED369, bouncetime=300)
-# print("Wait for the switch event.")
-#
-# while True:
-#     try:
-#         sleep(5)
-#     except KeyboardInterrupt:
-#         print("Au revoir!".center(20))
-#         GPIO.cleanup()
-#         break
+import keyboard
 
 btnPin = 18
 
@@ -24,7 +9,11 @@ GPIO.setup(btnPin, GPIO.IN, GPIO.PUD_UP)
 
 while True:
     GPIO.wait_for_edge(btnPin, GPIO.RISING, bouncetime=100)
-    time.sleep(0.1)
+    # time.sleep(0.1)
     #print(GPIO.input(btnPin))
     if GPIO.input(btnPin) == 0:
         print("switch pushed")
+
+    key = keyboard.read_key()
+    if key in ["q", "Q"]:
+        break
